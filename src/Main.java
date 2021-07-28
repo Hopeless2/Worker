d88e84f4bf79c9d6234e54f41e478affb2cb708a
Hopeless2 <89778966354@mail.ru>
2021-07-29 00:35:48 +0300

    Commit init

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args){
+        OnTaskDoneListener listenerDone = System.out::println;
+        OnTaskErrorListener listenerError = System.out::println;
+        Worker worker = new Worker(listenerDone, listenerError);
+        worker.start();
+    }
+}
